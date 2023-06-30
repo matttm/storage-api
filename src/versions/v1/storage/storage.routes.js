1f6f9724c4ceaf6f33a1cd3e9ad9fc0e75d92b60
matttm <matttmaloney@gmail.com>
2023-06-30 09:21:18 -0400

    creating controller

--- src/versions/v1/storage/storage.routes.js
+++ src/versions/v1/storage/storage.routes.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const StorageController = require('./storage,controller')
 
 const storageRoutes = express.Router();
 
-storageRoutes.get('presigned-url', null);
+storageRoutes.get('presigned-url', StorageController.getPresignedUrl);
 
 module.exports = storageRoutes;
