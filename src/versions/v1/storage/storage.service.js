05cf47d033bb121a9eff2c7d7748e22318e84a1c
matttm <matttmaloney@gmail.com>
2023-07-03 10:26:33 -0400

    adding creds to s3 req

--- src/versions/v1/storage/storage.service.js
+++ src/versions/v1/storage/storage.service.js
@@ -1,46 +1,47 @@
-const {config} = require("aws-sdk");
-const s3 = require('aws-sdk/clients/s3');
-
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 
 function StorageService() {
-    config.update({
-        region: process.env.AWS_REGION,
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    });
-    const s3Params = {
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: process.env.FILENAME,
-        Expires: 60 * 60,
-        ContentType: 'image/*'
-    };
-    function _getPresignedUrl(s3, s3Params) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                await s3.getSignedUrl('putObject', s3Params, function (err,         data) {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve(data);
-                });
-            } catch (error) {
-                return reject(error);
-            }
+  config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+  const s3Params = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: process.env.FILENAME,
+    Expires: 60 * 60,
+    ContentType: "image/*",
+  };
+  function _getPresignedUrl(s3, s3Params) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const client = new S3Client({
+          credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+          },
         });
-    }
-    const getPresignedUrl = async () => {
-        try {
-            const url = await _getPresignedUrl(s3, s3Params);
-            if (url) return url;
-            throw new Error('Error due to no url being returned');
-        } catch (e) {
-            console.error(e);
-            throw new Error('Error occurred getting presigned url');
-        }
-    };
-    return Object.freeze({
-        getPresignedUrl
+        const command = new GetObjectCommand(getObjectParams);
+        const url = await getSignedUrl(client, command, { expiresIn: 3600 });
+      } catch (error) {
+        return reject(error);
+      }
     });
+  }
+  const getPresignedUrl = async () => {
+    try {
+      const url = await _getPresignedUrl(s3, s3Params);
+      if (url) return url;
+      throw new Error("Error due to no url being returned");
+    } catch (e) {
+      console.error(e);
+      throw new Error("Error occurred getting presigned url");
+    }
+  };
+  return Object.freeze({
+    getPresignedUrl,
+  });
 }
 
 module.exports = StorageService();
